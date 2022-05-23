d11728218d5d133bfa6528c1e94a57b7a8de2bc5
M.srikanth srikanth <97142287+srikanth5599@users.noreply.github.com>
2022-05-23 04:17:07 +0000

    completed

--- ActivitySet#1/problem#11.py
+++ ActivitySet#1/problem#11.py
@@ -1,3 +1,26 @@
-# Tuples
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
 
-filename = "dataset/mbox-short.txt"
+email = dict()
+
+for line in handle :
+    if 'From:' in line:
+        line = line.strip()
+        line = line.split()
+        for word in line:
+            if '@' in word:
+                if word in email:
+                    email[word] = email[word] + 1
+                else:
+                    email[word] = 1
+
+largest = 0
+email_large = None
+
+for m in email :
+    if email[m] > largest:
+        largest = email[m]
+        email_large = m
+print(email_large, largest)
