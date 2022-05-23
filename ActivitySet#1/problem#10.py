cafdc002c03350a4703a01e6b72323f34cc05c01
M.srikanth srikanth <97142287+srikanth5599@users.noreply.github.com>
2022-05-23 04:15:58 +0000

    completed

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,17 @@
-# Dictionaries
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
 
-filename = "dataset/mbox-short.txt"
+fh = open(fname)
+count = 0
+
+for line in fh :
+    line = line.strip()
+    line = line.split()
+    if 'From' in line:
+        for word in line:
+            if '@' in word:
+                print(word)
+                count +=1
+
+print("There were", count, "lines in the file with From as the first word")
