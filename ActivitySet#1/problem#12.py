f69a95bffd59cfbedaa1a78922840632a206b25a
M.srikanth srikanth <97142287+srikanth5599@users.noreply.github.com>
2022-05-27 16:06:03 +0000

    completed

--- ActivitySet#1/problem#12.py
+++ ActivitySet#1/problem#12.py
@@ -1,2 +1,16 @@
 # Regular Expressions
 # https://www.py4e.com/lessons/regex
+
+import re
+
+hand = open("regex_sum_24962.txt")
+x=list()
+for line in hand:
+     y = re.findall('[0-9]+',line)
+     x = x+y
+
+sum=0
+for z in x:
+    sum = sum + int(z)
+
+print(sum)
